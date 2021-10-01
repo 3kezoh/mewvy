d8a2881bcab82cd94f129870cf404cf8dfe03762
Louis Pelarrey <louispelarrey@gmail.com>
2021-10-01 15:32:44 +0200

    :bug: Corrected duplicate default and json properties

--- app/Transformers/MoviesToDto.php
+++ app/Transformers/MoviesToDto.php
@@ -1,1 +1,20 @@
-<?php namespace App\Transformers;use App\Models\Movies;class MoviesToDto{public static function provideTransformer(Movies $movie){switch($movie->type){case'Comedy':return['id'=>$movie->id,'title'=>$movie->name,'category'=>'Comedy / Drama',];default:return['id'=>$movie->id,'title'=>$movie->name,'category'=>$movie->type,];case'Mystery/Crime':return['id'=>$movie->id,'title'=>$movie->name,'category'=>'Mystery / Crime',];default:return['id'=>$movie->id,'title'=>$movie->name,'category'=>$movie->type,];}}}
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Movies;
+
+class MoviesToDto
+{
+    public static function provideTransformer(Movies $movie)
+    {
+        switch ($movie->type) {
+            case 'Comedy':
+                return ['id' => $movie->id, 'name' => $movie->name, 'type' => 'Comedy / Drama',];
+            case 'Mystery/Crime':
+                return ['id' => $movie->id, 'name' => $movie->name, 'type' => 'Mystery / Crime',];
+            default:
+                return ['id' => $movie->id, 'name' => $movie->name, 'type' => $movie->type,];
+        }
+    }
+}
